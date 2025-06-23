97685f47552e7e93824d44c57d1afaf110482b47
Nikita Gamolsky <nikita.gamolsky@gmail.com>
2025-06-22 23:42:47 -0400

    fix migration again

---
+++ supabase/migrations/20250623034102_remote_schema.sql
@@ -0,0 +1,57 @@
+revoke delete on table "public"."leg_definitions" from "anon";
+
+revoke insert on table "public"."leg_definitions" from "anon";
+
+revoke references on table "public"."leg_definitions" from "anon";
+
+revoke select on table "public"."leg_definitions" from "anon";
+
+revoke trigger on table "public"."leg_definitions" from "anon";
+
+revoke truncate on table "public"."leg_definitions" from "anon";
+
+revoke update on table "public"."leg_definitions" from "anon";
+
+revoke delete on table "public"."placements" from "anon";
+
+revoke insert on table "public"."placements" from "anon";
+
+revoke references on table "public"."placements" from "anon";
+
+revoke select on table "public"."placements" from "anon";
+
+revoke trigger on table "public"."placements" from "anon";
+
+revoke truncate on table "public"."placements" from "anon";
+
+revoke update on table "public"."placements" from "anon";
+
+revoke delete on table "public"."results" from "anon";
+
+revoke insert on table "public"."results" from "anon";
+
+revoke references on table "public"."results" from "anon";
+
+revoke select on table "public"."results" from "anon";
+
+revoke trigger on table "public"."results" from "anon";
+
+revoke truncate on table "public"."results" from "anon";
+
+revoke update on table "public"."results" from "anon";
+
+revoke delete on table "public"."runners" from "anon";
+
+revoke insert on table "public"."runners" from "anon";
+
+revoke references on table "public"."runners" from "anon";
+
+revoke select on table "public"."runners" from "anon";
+
+revoke trigger on table "public"."runners" from "anon";
+
+revoke truncate on table "public"."runners" from "anon";
+
+revoke update on table "public"."runners" from "anon";
+
+
